d5b959e2a07cca60f627e7308a9632a01d6e187f
oz daniel <guzsbhtk@gmail.com>
2025-09-30 09:38:18 +0300

    מחיקת הודעה מיותר

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Basic Service Worker for offline support and runtime caching
-const CACHE_NAME = 'lpg-stations-v13';
+const CACHE_NAME = 'lpg-stations-v14';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
